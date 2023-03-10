9a42a9b6d8ffd32c5c9fc1f8537a1a06eebec365
HalesKozak <cclujes007@gmail.com>
2023-03-10 23:42:25 +0200

    Basic mechanics

---
+++ ClickerAndroid/Assets/Scripts/BuyLevel.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyLevel : MonoBehaviour
+{
+    [SerializeField] private GameManager _gameManager;
+
+    public int[] CostIntLevel;
+    public int[] CostBonus;
+    public Text [] CostText;
+
+    private void Start()
+    {
+        StartCoroutine(Bonus());
+    }
+    public void OnClickBuyLevelShop()
+    {
+        if(_gameManager.scoreCoins>= CostIntLevel[0])
+        {
+            _gameManager.scoreCoins -= CostIntLevel[0];
+            CostIntLevel[0] *= 2;
+            _gameManager.clickScore *= 2;
+            CostText[0].text = CostIntLevel[0]+" $";
+        }
+    }
+    public void OnClickBuyLevelBonus()
+    {
+        if (_gameManager.scoreCoins >= CostIntLevel[1])
+        {
+            _gameManager.scoreCoins -= CostIntLevel[1];
+            CostIntLevel[1] *= 2;
+            CostBonus[0] += 2*2;
+            CostText[1].text = CostIntLevel[1] + " $";
+        }
+    }
+
+    IEnumerator Bonus()
+    {
+        while (true)
+        {
+            _gameManager.scoreCoins += CostBonus[0];
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ ClickerAndroid/Assets/Scripts/GameManager.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] public int scoreCoins;
+    [SerializeField] private BuyLevel _buyLevel;
+
+    public GameObject shopPanel;
+    public GameObject bonusPanel;
+
+    private Save sv = new Save();
+
+    public Text scoreText;
+
+    public int clickScore=1;
+
+    private int TotalScoreCoinsBonus;
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("SV"))
+        {
+            sv = JsonUtility.FromJson<Save>(PlayerPrefs.GetString("SV"));
+
+            scoreCoins = sv.scoreCoins;
+            clickScore = sv.clickScore;
+
+            for (int i = 0; i < 1; i++)
+            {
+                _buyLevel.CostBonus[i] = sv.CostBonus[i];
+                TotalScoreCoinsBonus += sv.CostBonus[i];
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                _buyLevel.CostIntLevel[i] = sv.CostIntLevel[i];
+                _buyLevel.CostText[i].text = sv.CostIntLevel[i] + " $";
+            }
+        }
+    }
+
+    private void Start()
+    {
+        DateTime dateTime = new DateTime(sv.Date[0], sv.Date[1], sv.Date[2], sv.Date[3], sv.Date[4], sv.Date[5]);
+        TimeSpan timeSpan = DateTime.Now - dateTime;
+
+        scoreCoins += (int)timeSpan.TotalSeconds * TotalScoreCoinsBonus;
+    } 
+
+    void Update()
+    {
+        scoreText.text = scoreCoins+" $ ";
+    }
+
+    public void ShowAndHideShopPanel()
+    {
+        shopPanel.SetActive(!shopPanel.activeSelf);
+    }
+    public void ShowAndHideBonusPanel()
+    {
+        bonusPanel.SetActive(!bonusPanel.activeSelf);
+    }
+
+    public void OnclickButton()
+    {
+        scoreCoins += clickScore;
+    }
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    private void OnApplicationPause(bool pause)
+    {
+        sv.scoreCoins = scoreCoins;
+        sv.clickScore = clickScore;
+        sv.CostBonus = new int[1];
+        sv.CostIntLevel = new int[2];
+
+        for (int i = 0; i < 1; i++)
+        {
+            sv.CostBonus[i] = _buyLevel.CostBonus[i];
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            sv.CostIntLevel[i] = _buyLevel.CostIntLevel[i];
+        }
+
+        sv.Date[0] = DateTime.Now.Year; sv.Date[1] = DateTime.Now.Month; sv.Date[2] = DateTime.Now.Day;
+        sv.Date[3] = DateTime.Now.Hour; sv.Date[4] = DateTime.Now.Minute; sv.Date[5] = DateTime.Now.Second;
+
+        PlayerPrefs.SetString("SV", JsonUtility.ToJson(sv));
+    }
+#else
+    private void OnApplicationQuit()
+    {
+        sv.scoreCoins = scoreCoins;
+        sv.clickScore = clickScore;
+        sv.CostBonus = new int[1];
+        sv.CostIntLevel = new int[2];
+
+        for (int i = 0; i < 1; i++)
+        {
+            sv.CostBonus[i] = _buyLevel.CostBonus[i];
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            sv.CostIntLevel[i] = _buyLevel.CostIntLevel[i];
+        }
+
+        sv.Date[0] = DateTime.Now.Year; sv.Date[1] = DateTime.Now.Month; sv.Date[2] = DateTime.Now.Day;
+        sv.Date[3] = DateTime.Now.Hour; sv.Date[4] = DateTime.Now.Minute; sv.Date[5] = DateTime.Now.Second;
+
+        PlayerPrefs.SetString("SV", JsonUtility.ToJson(sv));
+    }
+#endif
+}
+
+[Serializable]
+public class Save
+{
+    public int scoreCoins;
+    public int clickScore;
+    public int [] CostIntLevel;
+    public int [] CostBonus;
+    public int[] Date = new int[6];
+}
